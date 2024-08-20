52e26b6ac3cef83991c2a6aad1d2aa333af462d3
Daniel88dev <daniel.hrynusiw@gmail.com>
2024-08-20 16:30:08 +0200

    Add logic to register user to API call

--- app/api/v1/register/route.ts
+++ app/api/v1/register/route.ts
@@ -1,8 +1,23 @@
 import { NextResponse } from "next/server";
+import { insertUser, User } from "@/drizzle/users";
 
 export async function POST(request: Request) {
   const response = await request.json();
-  console.log(response);
 
-  return NextResponse.json({ message: "success" }, { status: 200 });
+  if (response.type === "user.created") {
+    const userObject: User = {
+      clerkId: response.data.id,
+      name: response.data.first_name + " " + response.data.last_name,
+      email: response.data.email_addresses[0].email_address,
+    };
+
+    await insertUser(userObject);
+
+    return NextResponse.json({ message: "success" }, { status: 200 });
+  } else {
+    return NextResponse.json(
+      { error: "Error creating user." },
+      { status: 400 }
+    );
+  }
 }
