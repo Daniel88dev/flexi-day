e746e3ae95df4767a04e4b8cea320d21fafdfed6
Daniel88dev <daniel.hrynusiw@gmail.com>
2024-08-20 16:13:20 +0200

    updating register user API

--- app/api/v1/register/route.ts
+++ app/api/v1/register/route.ts
@@ -1,4 +1,8 @@
+import { NextResponse } from "next/server";
+
 export async function POST(request: Request) {
   const response = await request.json();
   console.log(response);
+
+  return NextResponse.json({ message: "success" }, { status: 200 });
 }
